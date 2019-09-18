e774f225c23448fed8a64cddd046822a20349ce6
Joe Horton <joseph.horton@digital.homeoffice.gov.uk>
2019-09-18 14:41:36 +0100

    blah

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Drone registration',
 
   // Default port that prototype runs on
   port: '3000',
